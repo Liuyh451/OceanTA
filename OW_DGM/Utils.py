修复了维度拓展和kl损失bug

OW_DGM/Utils.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -282,45 +283,28 @@ class Discriminator(nn.Module):
 
 
 # 定义CVAE模型（生成器部分）
-class CVAE(nn.Module):
-    def __init__(self, context_encoder, encoder, decoder):
-        super(CVAE, self).__init__()
-        self.context_encoder = context_encoder
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def forward(self, X, c):
-        # 从上下文编码器获得条件向量 c
-        c_encoded = self.context_encoder(c)
-
-        # 编码器输出潜在变量 z
-        z_mean, z_log_var = self.encoder(X, c_encoded)
-
-        # 使用重参数化技巧生成潜在变量 z
-        z = self.reparameterize(z_mean, z_log_var)
-
-        # 解码器生成重建的波浪场
-        X_hat = self.decoder(z, c_encoded)
-
-        return X_hat, z_mean, z_log_var
-
-    def reparameterize(self, mu, log_var):
-        std = torch.exp(0.5 * log_var)
+class Loss:
+    def reparameterize(self, mu, std):
         eps = torch.randn_like(std)
         return mu + eps * std
 
+    # 损失函数：对抗损失、重建损失、KL损失
+    def bce_loss(self,output, target):
+        criterion = nn.BCELoss()
+        return criterion(output, target)
 
-# 定义鉴别器
+    def mse_loss(self,X, X_hat):
+        return nn.MSELoss()(X, X_hat)
 
-# 损失函数：对抗损失、重建损失、KL损失
-def bce_loss(output, target):
-    criterion = nn.BCELoss()
-    return criterion(output, target)
+    def kl_loss(self,mu, sigma):
+        """
+            计算 KL 损失。
 
+            参数：
+            - mu: torch.Tensor，编码器输出的均值 (batch_size, latent_dim)。
+            - var: torch.Tensor，编码器输出的方差 (batch_size, latent_dim)。
 
-def mse_loss(X, X_hat):
-    return nn.MSELoss()(X, X_hat)
-
-
-def kl_loss(mu, log_var):
-    return -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+            返回：
+            - kl_loss: KL 损失，标量。
+            """
+        return 0.5 * torch.sum(mu.pow(2) + sigma.pow(2) - 1 - torch.log(sigma.pow(2))) / mu.size(1)

OW_DGM/buoy_data_prepare.py
@@ -119,6 +119,7 @@ def read_nc_files_and_extract_features(base_path, year_month):
 
         # 处理波浪数据
         cos_dirm, sin_dirm = process_wave_data(dirm)
+        print("cos,sin", cos_dirm, sin_dirm)
 
         # 将处理后的数据拼接为 (时间步, 特征数)
         data = np.stack([Hs, Tm, cos_dirm, sin_dirm], axis=-1)
@@ -127,8 +128,9 @@ def read_nc_files_and_extract_features(base_path, year_month):
         print(f"文件 {file} 特征维度处理后形状: {data.shape}")
         max_timestep = max(max_timestep, data.shape[0])  # 更新最大时间步
 
-    # 拼接为目标形状 (5, max_timestep, 3)
+    # 拼接为目标形状 (5, max_timestep, 4)
     data_array = np.stack(all_data, axis=0)
+    print("111", data_array[0, 0:3, 2], data_array[0, 0:3, 3])
     return data_array
 
 
@@ -186,7 +188,7 @@ def normalize_and_save_numpy(data, save_path):
 
     # 归一化前两维特征
     normalized_data_np = np.zeros_like(data)
-    for i in range(2):  # 遍历每个特征
+    for i in range(4):  # 遍历每个特征
         feature = data[:, :, i]  # 提取当前特征
 
         # 忽略 NaN 值，计算最小值和最大值
@@ -199,7 +201,10 @@ def normalize_and_save_numpy(data, save_path):
 
         # 归一化公式
         normalized_feature = 2 * (feature - min_val) / (max_val - min_val) - 1
-        normalized_data_np[:, :, i] = normalized_feature
+        if (i > 2):
+            normalized_data_np[:, :, i] = feature
+        else:
+            normalized_data_np[:, :, i] = normalized_feature
 
     # 转回 PyTorch
     normalized_data = torch.from_numpy(normalized_data_np)
@@ -274,18 +279,33 @@ def reset_indices(arr):
 base_path = r"E:\Dataset\met_waves\buoy"
 save_path = "./data"
 start_year = 2017
-end_year = 2020
+end_year = 2021
 combined_data = combine_monthly_data(base_path, start_year, end_year)
 print(f"Combined data shape: {combined_data.shape}")
 np_data = combined_data.numpy()
+# column_data=np_data[:,:,3]
+# column_data= column_data[~np.isnan(column_data)]
+# max_value = column_data.max()
+# min_value = column_data.min()
+# print(np_data.shape,max_value,min_value)
 sequence_length = np_data.shape[1]  # 获取 b 的长度
 # 动态生成索引：每次提取连续 3 个点，跳过 3 个点
 indices_to_extract = generate_indices(sequence_length, group_size=3, skip_size=3)
 # 提取指定索引的数据
 extracted_data = extract_elements(np_data, indices_to_extract)
 print("提取后的数据形状：", extracted_data.shape)
+column_data = extracted_data[:, :, 3]
+column_data = column_data[~np.isnan(column_data)]
+max_value = column_data.max()
+min_value = column_data.min()
+print(extracted_data.shape, max_value, min_value)
 # 重置索引（这里的 "索引" 是 NumPy 数组本身的顺序）
 reset_data = reset_indices(extracted_data)
+column_data = reset_data[:, :, 3]
+column_data = column_data[~np.isnan(column_data)]
+max_value = column_data.max()
+min_value = column_data.min()
+print(reset_data.shape, max_value, min_value)
 # 创建保存路径
 os.makedirs(save_path, exist_ok=True)
 # 调用函数

OW_DGM/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import wave_filed_data_prepare
 import Utils
 from Utils import *
 import torch
@@ -52,30 +54,12 @@ class TimeSeriesDataset(Dataset):
 
         return A_batches, B_batches
 
-# 示例数据
-T1 = 300  # 时间步
-T2 =100
-A = torch.randn(5, T1, 4)  # (5, T, 4)
-B = torch.randn(T2, 4, 128, 128)  # (T, 4, 128, 128)
-batch_size = 10
-
-# 创建数据集和数据加载器
-dataset = TimeSeriesDataset(A, B, batch_size=batch_size)
-dataloader = DataLoader(dataset, batch_size=None, shuffle=False)
-
-# 测试
-for i, (A_batch, B_batch) in enumerate(dataloader):
-    print(f"Batch {i + 1}:")
-    print(f"A_batch shape: {A_batch.shape}")
-    print(f"B_batch shape: {B_batch.shape}")
-
-
 
 def train_cvae_al(
-    context_encoder, encoder, decoder, discriminator,
-    dataloader, device, latent_dim=128,
-    lambda_kl=0.1, lambda_adv=0.01, lr=1e-4,
-    epochs=50, verbose=True
+        context_encoder, encoder, decoder, discriminator,
+        dataloader, device, latent_dim=128,
+        lambda_kl=0.1, lambda_adv=0.01, lr=1e-4,
+        epochs=50, verbose=True
 ):
     """
     训练 CVAE-AL 模型的函数。
@@ -98,6 +82,7 @@ def train_cvae_al(
     - context_encoder, encoder, decoder, discriminator：训练后的模型。
     """
     # 损失函数
+    loss = Loss()
     mse_loss = nn.MSELoss()  # 重构损失
     bce_loss = nn.BCELoss()  # 二分类交叉熵损失
 
@@ -123,6 +108,8 @@ def train_cvae_al(
         discriminator.train()
 
         for batch_idx, (buoy_data, wave_field) in enumerate(dataloader):
+            print(f"Batch {batch_idx + 1}:", f"buoy_data_batch shape: {buoy_data.shape}",
+                  f"wave_field_batch shape: {wave_field.shape}")
             # 将数据移动到设备
             buoy_data = buoy_data.to(device)  # 浮标数据 (batch_size, 5, 3, 4)
             wave_field = wave_field.to(device)  # 波场数据 (batch_size, 3, 128, 128)
@@ -131,23 +118,21 @@ def train_cvae_al(
             context_vector = context_encoder(buoy_data)  # (batch_size, latent_dim)
 
             # 编码器生成 z 的均值和标准差
-            z_mean, z_logvar = encoder(wave_field, context_vector)  # (batch_size, latent_dim)
-            # # 从标准正态分布中采样一个随机张量 eps
-            # eps = torch.randn_like(mu)
-            #
-            # # 计算 Z
-            # Z = mu + sigma * eps
-
-            z_std = torch.exp(0.5 * z_logvar)
-            z = torch.randn_like(z_std) * z_std + z_mean  # 重参数化采样
+            z_mean, z_var = encoder(wave_field, context_vector)  # (batch_size, latent_dim)
+            z = loss.reparameterize(z_mean, z_var)  # 重参数化采样
             # 解码器生成波场
             reconstructed_wave_field = decoder(z, context_vector)  # (batch_size, 3, 128, 128)
-            # 计算生成器损失
-            l_rec = mse_loss(reconstructed_wave_field, wave_field)  # 重构损失
-            kl_div = -0.5 * torch.sum(1 + z_logvar - z_mean.pow(2) - torch.exp(z_logvar)) / wave_field.size(0)
+            # 重构损失
+            l_rec = loss.mse_loss(wave_field, reconstructed_wave_field)
+            # KL损失
+            l_kl = loss.kl_loss(z_mean, z_var)
             fake_preds = discriminator(reconstructed_wave_field)  # 判别器对生成波场的判别
-            l_adv_G = bce_loss(fake_preds, torch.ones_like(fake_preds))  # 对抗损失（生成器）
-            l_G = l_rec + lambda_kl * kl_div + lambda_adv * l_adv_G  # 总生成器损失
+            print("Fake predictions range:", fake_preds.min().item(), fake_preds.max().item())
+            l_adv_G = loss.bce_loss(fake_preds, torch.ones_like(fake_preds))  # 对抗损失（生成器）
+            # print("Real predictions range:", real_preds.min().item(), real_preds.max().item())
+
+
+            l_G = l_rec + lambda_kl * l_kl + lambda_adv * l_adv_G  # 总生成器损失
             # 优化生成器
             optimizer_G.zero_grad()
             l_G.backward()
@@ -170,12 +155,72 @@ def train_cvae_al(
             if verbose:
                 print(f"Epoch [{epoch + 1}/{epochs}] Batch [{batch_idx + 1}/{len(dataloader)}]: "
                       f"Loss_G: {l_G.item():.4f}, Loss_D: {l_adv_D.item():.4f}, "
-                      f"L_rec: {l_rec.item():.4f}, KL: {kl_div.item():.4f}")
+                      f"L_rec: {l_rec.item():.4f}, KL: {l_kl.item():.4f}")
 
     return context_encoder, encoder, decoder, discriminator
+
+
+def replace_invalid_values(data):
+    """
+    将数据中值为 -32768、9999.0 或 NaN 的位置替换为 0。
+
+    参数：
+    - data: torch.Tensor，输入数据张量。
+
+    返回：
+    - 处理后的 torch.Tensor。
+    """
+    # 替换 NaN 值为 0
+    data = torch.nan_to_num(data, nan=0.0)
+    # 替换 -32768 和 9999.0 为 0
+    data = torch.where((data == -32768) | (data == 9999.0), 0.0, data)
+    return data
+
+
+def count_nan_values(data):
+    """
+    统计数据中 NaN 值的总数。
+
+    参数：
+    - data: torch.Tensor，输入数据张量。
+
+    返回：
+    - NaN 值的数量。
+    """
+    return torch.isnan(data).sum().item()
+
+def normalize(data):
+    x_min = torch.min(data)
+    x_max = torch.max(data)
+    data = (data - x_min) / (x_max - x_min)
+    return data
 # 定义设备
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+print("loading swan data and buoy data........")
+swan_data= torch.randn(270, 4, 128, 128)  # (T, 4, 128, 128)
+buoy_data= torch.randn( 5, 810, 4)
+swan_data=normalize(swan_data)
+buoy_data=normalize(buoy_data)
+# swan_data = np.load("E:/Dataset/met_waves/swan_data.npy")
+# buoy_data = np.load('data/buoy_obs.npy')
+print(torch.min(buoy_data), torch.max(buoy_data))
+buoy_data = torch.tensor(buoy_data)
+# swan_data = wave_filed_data_prepare.combine_monthly_data("/home/hy4080/met_waves/Swan_cropped/swanSula", 2017, 2018)
+# swan_data = torch.tensor(swan_data).permute(0, 3, 1, 2)
+# 处理 Swan 数据
+swan_data = replace_invalid_values(swan_data)
+# 查看 Swan 数据中的 NaN 值数量
+# 处理 Buoy 数据
+buoy_data = replace_invalid_values(buoy_data)
+num_nan_swan = count_nan_values(swan_data)
+print(f"Swan 数据中 NaN 值的数量: {num_nan_swan}")
+# 查看 Buoy 数据中的 NaN 值数量
+num_nan_buoy = count_nan_values(buoy_data)
+print(f"Buoy 数据中 NaN 值的数量: {num_nan_buoy}")
+print("swan data and buoy data shape", buoy_data.shape, swan_data.shape)
+# 创建数据集和数据加载器
+dataset = TimeSeriesDataset(buoy_data, swan_data, batch_size=32)
+dataloader = DataLoader(dataset, batch_size=None, shuffle=False)
 # 训练模型
 context_encoder = Utils.ContextualEncoder()
 encoder = Utils.WaveFieldEncoderWithBuoy()
@@ -187,5 +232,3 @@ trained_context_encoder, trained_encoder, trained_decoder, trained_discriminator
     lambda_kl=0.1, lambda_adv=0.01, lr=1e-4,
     epochs=50, verbose=True
 )
-
-

OW_DGM/wave_filed_data_prepare.py
@@ -61,10 +61,10 @@ def combine_monthly_data(base_path, start_year, end_year):
     """
     monthly_data = []
 
-    for year in range(start_year, end_year + 1):
+    for year in range(start_year, end_year):
         for month in range(1, 13):  # 遍历1到12月
             year_month = f"{year}{month:02d}"  # 格式化为"YYYYMM"
-            if (year == 2019 and month > 2):
+            if (year == 2020 and month > 2):
                 break
             # 检查文件是否存在
             # 调用函数读取数据
@@ -79,7 +79,5 @@ def combine_monthly_data(base_path, start_year, end_year):
         raise ValueError("No valid data files found for the specified range.")
 
     # 拼接所有月份的数据
-    combined_data = torch.cat(monthly_data, dim=1)  # 按时间步拼接 (num_buoys, total_time_steps, feature_dim)
+    combined_data = torch.cat(monthly_data, dim=0)  # 按时间步拼接 (num_buoys, total_time_steps, feature_dim)
     return combined_data
-
-combined_data=combine_monthly_data("E:/Dataset/met_waves/Swan_cropped/swanSula",2017,2021)